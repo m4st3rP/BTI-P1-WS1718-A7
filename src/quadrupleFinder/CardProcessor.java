47725e108c02827c3833939a2c20218f7242fb49
Philipp Schwarz <schwarz_philipp@hotmail.de>
2017-12-14 02:35:42 +0100

    added CardProcessor

---
+++ src/quadrupleFinder/CardProcessor.java
@@ -0,0 +1,16 @@
+package quadrupleFinder;
+
+public class CardProcessor implements CardProcessor_I {
+    
+    public CardProcessor() {
+        
+    }
+    
+    public boolean process(Card card) {
+        return false;
+    }
+    
+    public void reset() {
+        
+    }
+}
